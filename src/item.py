a261c74b683cad80839727dd64f9eed477271435
marinaprojects <155964514+marinaprojects@users.noreply.github.com>
2024-03-03 21:38:12 -0600

    add item.py

--- src/item.py
+++ src/item.py
@@ -1,7 +1,11 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
+    DATA_DIR = '../src/items.csv'
     pay_rate = 1.0
     all = []
 
@@ -13,7 +17,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
@@ -32,3 +36,34 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        """добавить геттер"""
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        """добавить сеттер
+        длина наименования товара не больше 10 симвовов"""
+        self.__name = value[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """класс-метод, инициализирующий экземпляры класса
+        `Item` данными из файла _src/items.csv_"""
+        with open(cls.DATA_DIR, newline='', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            cls.all.clear()
+            for row in reader:
+                #print(row['name'], row['price'], row['quantity'])
+                name = row['name']
+                price = row['price']
+                quantity = row['quantity']
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(param):
+        """статический метод, возвращающий число из числа-строки"""
+        return int(float(param))
+
