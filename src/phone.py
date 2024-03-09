4199ef7c1f81babda3c5a8232c788c304d64f368
marinaprojects <155964514+marinaprojects@users.noreply.github.com>
2024-03-08 20:52:27 -0600

    fixed phone.py

--- src/phone.py
+++ src/phone.py
@@ -20,6 +20,15 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
+    @property
+    def number_of_sim(self) -> int:
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value: int) -> None:
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Количество SIM-карт должно быть целым числом больше нуля.")
+        self.__number_of_sim = value
 
     def __add__(self, other):
         """добавление количества экземпляров телефона и товара"""
